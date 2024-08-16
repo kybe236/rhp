c4dc02d17319505e1641f89465e0f31388bb1b95
Kyrill Belomenov <apfel236@outlook.de>
2024-08-16 23:00:38 +0200

    added plugin parser

--- src/plugin.zig
+++ src/plugin.zig
@@ -35,7 +35,16 @@ const Plugin = struct {
     name: std.ArrayList(u8),
     description: std.ArrayList(u8),
     url: std.ArrayList(u8),
-    img: std.ArrayList(u8),
+    creator: std.ArrayList(u8),
+    creatorLink: std.ArrayList(u8),
+
+    pub fn deinit(self: *Plugin) void {
+        self.name.deinit();
+        self.description.deinit();
+        self.url.deinit();
+        self.creator.deinit();
+        self.creatorLink.deinit();
+    }
 };
 
 const Scraper = struct {
@@ -56,7 +65,7 @@ const Scraper = struct {
     }
 
     pub fn scrape(self: *Scraper) !void {
-        std.debug.print("[+] scraping started", .{});
+        std.debug.print("[+] scraping started\n", .{});
         var client = std.http.Client{
             .allocator = self.allocator,
         };
@@ -78,22 +87,166 @@ const Scraper = struct {
         }
 
         var lines = std.mem.splitAny(u8, response.items, "\n");
-        defer lines.deinit();
+
+        var plugin_lines = std.ArrayList(u8).init(self.allocator);
+        defer plugin_lines.deinit();
 
         var at_start = true;
-        while (true) {
-            var line = lines.next();
-            if (line == null) {
-                continue;
-            }
-            line = line.?;
+        while (lines.peek() != null) {
+            const line = lines.next().?;
             if (eql(u8, line, "<!-- START PLUGINS LIST -->")) {
                 at_start = false;
+                continue;
+            }
+            if (at_start) {
                 continue;
             }
             if (eql(u8, line, "<!-- END PLUGINS LIST -->")) {
                 break;
             }
+            try plugin_lines.appendSlice(line);
+            try plugin_lines.appendSlice("\n");
+        }
+
+        std.debug.print("[+] found {d} plugins\n", .{1 + std.mem.count(u8, plugin_lines.items, "---")});
+
+        var entries = std.mem.splitSequence(u8, plugin_lines.items, "---");
+
+        while (entries.peek() != null) {
+            const plugin = entries.next();
+            if (plugin == null) {
+                break;
+            }
+
+            var name: ?[]const u8 = null;
+            var githubLink: ?[]const u8 = null;
+            var creator: ?[]const u8 = null;
+            var creatorLink: ?[]const u8 = null;
+            var description: ?[]const u8 = null;
+
+            var plugin_lin = std.mem.splitAny(u8, plugin.?, "\n");
+
+            while (plugin_lin.peek() != null) {
+                var line = plugin_lin.next();
+                if (line == null) {
+                    break;
+                }
+                if (line.?.len == 0) {
+                    continue;
+                }
+
+                if (std.mem.startsWith(u8, line.?, "### [")) {
+                    const nameStart = std.mem.indexOf(u8, line.?, "[");
+                    const nameEnd = std.mem.indexOf(u8, line.?, "](https://");
+
+                    if (nameStart != null and nameEnd != null and nameStart.? < nameEnd.?) {
+                        name = line.?[nameStart.? + 1 .. nameEnd.?];
+                    }
+
+                    const linkStart = std.mem.indexOf(u8, line.?, "https://");
+                    const linkEnd = std.mem.indexOf(u8, line.?, ") <br>");
+
+                    if (linkStart != null and linkEnd != null and linkStart.? < linkEnd.?) {
+                        githubLink = line.?[linkStart.?..linkEnd.?];
+                    }
+                } else if (std.mem.startsWith(u8, line.?, "**Creator**: ")) {
+                    const creatorStart = std.mem.indexOf(u8, line.?, "> [");
+                    const creatorEnd = std.mem.indexOf(u8, line.?, "](https://");
+                    if (creatorStart != null and creatorEnd != null and creatorStart.? < creatorEnd.?) {
+                        creator = line.?[creatorStart.? + 3 .. creatorEnd.?];
+                    }
+                    const creatorLinkStart = std.mem.indexOf(u8, line.?, "](https://");
+                    const creatorLinkEnd = std.mem.indexOf(u8, line.?, ")");
+                    if (creatorLinkStart != null and creatorLinkEnd != null and creatorLinkStart.? < creatorLinkEnd.?) {
+                        creatorLink = line.?[creatorLinkStart.? + 2 .. creatorLinkEnd.?];
+                    }
+                } else if (std.mem.containsAtLeast(u8, line.?, 1, "<details>") or
+                    std.mem.containsAtLeast(u8, line.?, 1, "</details>") or
+                    std.mem.containsAtLeast(u8, line.?, 1, "<summary>") or
+                    std.mem.containsAtLeast(u8, line.?, 1, "</summary>") or
+                    std.mem.containsAtLeast(u8, line.?, 1, "<p align=\"center\">") or
+                    std.mem.containsAtLeast(u8, line.?, 1, "</p>") or
+                    std.mem.containsAtLeast(u8, line.?, 1, "<img src=") or
+                    std.mem.containsAtLeast(u8, line.?, 1, "</a>") or
+                    std.mem.containsAtLeast(u8, line.?, 1, "<a href="))
+                {
+                    continue;
+                } else if (line.?.len > 1) {
+                    description = line.?;
+                }
+            }
+
+            // Automatically report missing information
+            if (name == null or githubLink == null or creator == null or description == null or creatorLink == null) {
+                std.debug.print("[-] Detected missing information\n", .{});
+                std.debug.print("[-] Please contact maintainer\n", .{});
+                if (name == null) {
+                    std.debug.print("[-] Missing name\n", .{});
+                }
+                if (githubLink == null) {
+                    std.debug.print("[-] Missing github link\n", .{});
+                }
+                if (creator == null) {
+                    std.debug.print("[-] Missing creator\n", .{});
+                }
+                if (description == null) {
+                    std.debug.print("[-] Missing description\n", .{});
+                }
+                if (creatorLink == null) {
+                    std.debug.print("[-] Missing creator link\n", .{});
+                }
+                if (name != null) {
+                    std.debug.print("[-] Name: {s}\n", .{name.?});
+                }
+                if (githubLink != null) {
+                    std.debug.print("[-] Github link: {s}\n", .{githubLink.?});
+                }
+                if (creator != null) {
+                    std.debug.print("[-] Creator: {s}\n", .{creator.?});
+                }
+                if (description != null) {
+                    std.debug.print("[-] Description: {s}\n", .{description.?});
+                }
+                if (creatorLink != null) {
+                    std.debug.print("[-] Creator link: {s}\n", .{creatorLink.?});
+                }
+            }
+
+            var plugin_struct = Plugin{
+                .name = std.ArrayList(u8).init(self.allocator),
+                .description = std.ArrayList(u8).init(self.allocator),
+                .url = std.ArrayList(u8).init(self.allocator),
+                .creator = std.ArrayList(u8).init(self.allocator),
+                .creatorLink = std.ArrayList(u8).init(self.allocator),
+            };
+
+            if (name != null) {
+                try plugin_struct.name.appendSlice(name.?);
+            }
+            if (githubLink != null) {
+                try plugin_struct.url.appendSlice(githubLink.?);
+            }
+            if (creator != null) {
+                try plugin_struct.creator.appendSlice(creator.?);
+            }
+            if (description != null) {
+                try plugin_struct.description.appendSlice(description.?);
+            }
+            if (creatorLink != null) {
+                try plugin_struct.creatorLink.appendSlice(creatorLink.?);
+            }
+
+            defer plugin_struct.deinit();
+
+            
+
+            std.debug.print("[+] Found plugin: {s}\n Description: {s}\n Creator: {s}\n CreatorLink: {s}\n Github: {s}\n", .{
+                plugin_struct.name.items,
+                plugin_struct.description.items,
+                plugin_struct.creator.items,
+                plugin_struct.creatorLink.items,
+                plugin_struct.url.items,
+            });
         }
     }
 };
