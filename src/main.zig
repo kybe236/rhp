397d0138c35e0f29adcdb525464eb9391a34a4ca
Kyrill Belomenov <apfel236@outlook.de>
2024-08-18 13:34:50 +0200

    better loging

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,7 @@ const leakmsg =
 ;
 
 pub var log_level: std.log.Level = .info;
+pub const main_log = std.log.scoped(.main);
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -30,12 +31,44 @@ pub fn main() !void {
     }
 
     // Print arguments
-    std.debug.print("Arguments: ", .{});
-    for (args) |arg| {
-        std.debug.print("{s} ", .{arg});
+    main_log.debug("Arguments: ", .{});
+    if (std.options.log_level == .debug) {
+        for (args) |arg| {
+            std.debug.print("{s} ", .{arg});
+        }
+        std.debug.print("\n", .{});
     }
-    std.debug.print("\n", .{});
 
     // Call cli code
     try cli.handle(allocator, args);
+}
+
+pub const std_options = std.Options{
+    .logFn = log,
+};
+
+pub fn log(
+    comptime message_level: std.log.Level,
+    comptime scope: @Type(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    const scope_txt = "(" ++ @tagName(scope) ++ ") ";
+    const stderr = std.io.getStdErr().writer();
+    var bw = std.io.bufferedWriter(stderr);
+    const writer = bw.writer();
+
+    const lvl_txt = switch (message_level) {
+        .debug => "[DEBUG] ",
+        .info => "[+] ",
+        .warn => "[-] ",
+        .err => "[ERROR] ",
+    };
+
+    std.debug.lockStdErr();
+    defer std.debug.unlockStdErr();
+    nosuspend {
+        writer.print(scope_txt ++ lvl_txt ++ format, args) catch return;
+        bw.flush() catch return;
+    }
 }
